0eb4e22dd3dae395d25aec47c32cbb0ae1cb8299
adelyasser00 <adelyasser00@gmail.com>
2022-10-04 02:02:09 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -5,38 +5,40 @@ const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const API_KEY='&appid=3734059046e3ec7ec64c53f70125cac5&units=imperial';
 
 // Create a new date instance dynamically with JS
-
-let d = new Date();
-let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = new Date();
+let newDate = date.getMonth()+1+'.'+ date.getDate()+'.'+ date.getFullYear();
 // Add event listener to generate button
-document.getElementById('generate').addEventListener('click',performAction);
-function performAction(e){
+document.getElementById('generate').addEventListener('click',()=>{
   // First we get the zip code and the feelings from the user
   const ZIP = document.getElementById('zip').value;
   const FEELINGS = document.getElementById('feelings').value;
-  getWeather(BASE_URL,ZIP,API_KEY)
+  generateWeather(BASE_URL,ZIP,API_KEY)
   // then we dynamically update the UI with the new data
-  .then(function(data){
-    console.log(data);
-    postData('/add',{date:newDate,temp:data.main.temp,content:FEELINGS});
+  .then(function(weather){
+    console.log(weather);
+    // add data to POST request
+    postData('/add',{date:newDate,temp:weather.main.temp,content:FEELINGS});
+    // Update UI with new data obtained
     updateUI();
   })
-}
+});
+
  const updateUI = async () => {
   // get the data from the server
   const request = await fetch('/all');
   try{
     // convert the data to json
-    const allData = await request.json();
+    const updatedData = await request.json();
     // update the UI
-    document.getElementById('date').innerHTML = allData.date;
-    document.getElementById('temp').innerHTML = allData.temp;
-    document.getElementById('content').innerHTML = allData.content;
+    document.getElementById('date').innerHTML = updatedData.date;
+    document.getElementById('temp').innerHTML = updatedData.temp;
+    document.getElementById('content').innerHTML = updatedData.content;
   }catch(error){
     console.log("error",error);
   }
 }
 const postData = async ( url = '', data = {})=>{
+  // body of the POST request, as taught in the course
     const response = await fetch(url, {
     method: 'POST', 
     credentials: 'same-origin', 
@@ -44,10 +46,11 @@ const postData = async ( url = '', data = {})=>{
         'Content-Type': 'application/json',
     },
     body: JSON.stringify({
+      // convert the data used to strings
       date: data.date,
       temp: data.temp,
       content: data.content
-    }), // body data type must match "Content-Type" header        
+    }),       
   });
     try {
       // convert the data to json
@@ -58,10 +61,10 @@ const postData = async ( url = '', data = {})=>{
     }
 };
 // get the weather data from the API using the zip code and the API key
-const getWeather = async (BASE_URL,ZIP,API_KEY)=>{
-  const res = await fetch(BASE_URL+ZIP+API_KEY)
+const generateWeather = async (BASE_URL,ZIP,API_KEY)=>{
+  const result = await fetch(BASE_URL+ZIP+API_KEY)
   try {
-    const data = await res.json();
+    const data = await result.json();
     return data;
   }  catch(error) {
     console.log("error", error);
